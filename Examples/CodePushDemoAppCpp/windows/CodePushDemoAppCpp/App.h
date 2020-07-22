9959767be13038181745e7ba01832d08b8a57927
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-07-22 16:09:01 -0700

    Create project CodePushDemoAppCpp.

---
+++ Examples/CodePushDemoAppCpp/windows/CodePushDemoAppCpp/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::CodePushDemoAppCpp::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::CodePushDemoAppCpp::implementation
+
+
