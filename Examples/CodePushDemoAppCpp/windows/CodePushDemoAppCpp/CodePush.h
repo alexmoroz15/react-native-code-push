4e5a45a4552f6b91c69d79b66022f792c99caa46
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-09-30 18:18:58 -0700

    Add, modify, and rearrange module methods exported to JavaScript.

--- Examples/CodePushDemoAppCpp/windows/CodePushDemoAppCpp/CodePush.h
+++ Examples/CodePushDemoAppCpp/windows/CodePushDemoAppCpp/CodePush.h
@@ -6,6 +6,7 @@
 
 namespace CodePush
 {
+    /*
 	using JsonObject = winrt::Windows::Data::Json::JsonObject;
 	using path = std::filesystem::path;
 	using wstring = std::wstring;
@@ -14,6 +15,14 @@ namespace CodePush
 	using ReactContext = winrt::Microsoft::ReactNative::ReactContext;
 
 	using DateTime = winrt::Windows::Foundation::DateTime;
+    */
+	using namespace winrt;
+	using namespace Windows::Data::Json;
+	using namespace Windows::Foundation;
+	using namespace Microsoft::ReactNative;
+
+	using namespace std;
+	using namespace filesystem;
 
 	REACT_MODULE(CodePush);
 	struct CodePush
@@ -21,38 +30,6 @@ namespace CodePush
 		enum class CodePushInstallMode;
 
 	private:
-		/*
-		const std::wstring PendingUpdatekey{ L"CODE_PUSH_PENDING_UPDATE" };
-
-		const std::wstring PendingUpdateHashKey{ L"hash" };
-		const std::wstring PendingUpdateIsLoadingKey{ L"isLoading" };
-		const winrt::hstring DefaultJSBundleName{ L"index.windows" };
-		const winrt::hstring AssetsBundlePrefix{ L"ms-appx:///" };
-		
-		bool _allowed{ true };
-		bool _restartInProgress{ false };
-		static inline bool _testConfigurationFlag{ false };
-		std::vector<uint8_t> _restartQueue;
-
-		winrt::Microsoft::ReactNative::ReactNativeHost m_host;
-		winrt::Microsoft::ReactNative::ReactContext m_context;
-
-		winrt::hstring m_assetsBundleFileName;
-		winrt::hstring m_ClientUniqueId;
-		std::string m_BinaryContentsHash{ "" };
-
-		static bool IsUsingTestConfiguration();
-		static void SetUsingTestConfiguration(bool shouldUseTestConfiguration);
-
-		winrt::fire_and_forget LoadBundle();
-		void RestartAppInternal(bool onlyIfUpdateIsPending);
-		bool IsPendingUpdate(winrt::hstring&& updateHash);
-
-		winrt::Windows::Foundation::IAsyncOperation<winrt::hstring> GetJSBundleFile();
-		winrt::Windows::Foundation::IAsyncOperation<winrt::hstring> GetJSBundleFile(winrt::hstring assetsBundleFileName);
-		void SetJSBundleFile(winrt::hstring latestJSBundleFile);
-		*/
-
 		bool m_hasResumeListener;
 		bool m_isFirstRunAfterUpdate;
 		int m_minimumBackgroundDuration;
@@ -136,7 +113,7 @@ namespace CodePush
 		};
 
 		static path GetBinaryBundlePath();
-		static winrt::Windows::Foundation::IAsyncOperation<path> GetBundlePathAsync();
+		static IAsyncOperation<path> GetBundlePathAsync();
 		static path GetBundlePath();
 
 		// Not sure exactly why these methods exist
@@ -177,63 +154,105 @@ namespace CodePush
 		}
 
 		/*
-		// Synchronous functions are not allowed on the UI thread.
-		// Incidentally, the application is initialized and the bundle loaded by the UI thread.
-		static winrt::hstring GetJSBundleFileSync();
+		 * This is native-side of the RemotePackage.download method
+		 */
+		REACT_METHOD(DownloadUpdate, L"downloadUpdate");
+		void DownloadUpdate(JsonObject updatePackage, bool notifyProgress, ReactPromise<JsonObject> promise) noexcept;
 
-		REACT_CONSTANT_PROVIDER(GetConstants);
-		void GetConstants(winrt::Microsoft::ReactNative::ReactConstantProvider& constants) noexcept
-		{
-			constants.Add(L"codePushInstallModeImmediate", CodePushInstallMode::IMMEDIATE);
-			constants.Add(L"codePushInstallModeOnNextRestart", CodePushInstallMode::ON_NEXT_RESTART);
-			constants.Add(L"codePushInstallModeOnNextResume", CodePushInstallMode::ON_NEXT_RESUME);
-			constants.Add(L"codePushInstallModeOnNextSuspend", CodePushInstallMode::ON_NEXT_SUSPEND);
+        /*
+         * This is the native side of the CodePush.getConfiguration method. It isn't
+         * currently exposed via the "react-native-code-push" module, and is used
+         * internally only by the CodePush.checkForUpdate method in order to get the
+         * app version, as well as the deployment key that was configured in the Info.plist file.
+         */
+		REACT_METHOD(GetConfiguration, L"getConfiguration");
+		void GetConfiguration(ReactPromise<JsonObject> promise) noexcept;
 
-			constants.Add(L"codePushUpdateStateRunning", CodePushUpdateState::RUNNING);
-			constants.Add(L"codePushUpdateStatePending", CodePushUpdateState::PENDING);
-			constants.Add(L"codePushUpdateStateLatest", CodePushUpdateState::LATEST);
-		}
+        /*
+         * This method is the native side of the CodePush.getUpdateMetadata method.
+         */
+		REACT_METHOD(GetUpdateMetadata, L"getUpdateMetadata");
+        void GetUpdateMetadata(CodePushUpdateState updateState, ReactPromise<JsonObject> promise) noexcept;
 
-		REACT_INIT(Initialize);
-		void Initialize(winrt::Microsoft::ReactNative::ReactContext const& reactContext) noexcept;
+        /*
+         * This method is the native side of the LocalPackage.install method.
+         */
+		REACT_METHOD(InstallUpdate, L"installUpdate");
+        void InstallUpdate(JsonObject updatePackage, CodePushInstallMode installMode, int minimumBackgroundDuration, ReactPromise<void> promise) noexcept;
+
+        /*
+         * This method isn't publicly exposed via the "react-native-code-push"
+         * module, and is only used internally to populate the RemotePackage.failedInstall property.
+         */
+		REACT_METHOD(IsFirstRun, L"isFirstRun");
+        void IsFailedUpdate(wstring packageHash, ReactPromise<bool> promise) noexcept;
+
+		REACT_METHOD(SetLatestRollbackInfo, L"setLatestRollbackInfo");
+        void SetLatestRollbackInfo(wstring packageHash) noexcept;
+
+		REACT_METHOD(GetLatestRollbackInfo, L"getLatestRollbackInfo");
+        void GetLatestRollbackInfo(ReactPromise<JsonObject> promise) noexcept;
+
+        /*
+         * This method isn't publicly exposed via the "react-native-code-push"
+         * module, and is only used internally to populate the LocalPackage.isFirstRun property.
+         */
+		REACT_METHOD(IsFirstRun, L"isFirstRun");
+        void IsFirstRun(wstring packageHash, ReactPromise<bool> promise) noexcept;
+
+        /*
+         * This method is the native side of the CodePush.notifyApplicationReady() method.
+         */
+		REACT_METHOD(NotifyApplicationReady, L"notifyApplicationReady");
+        void NotifyApplicationReady() noexcept;
 
 		REACT_METHOD(Allow, L"allow");
-		void Allow(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(Disallow, L"disallow");
-		void Disallow(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(GetConfiguration, L"getConfiguration");
-		void GetConfiguration(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(GetNewStatusReport, L"getNewStatusReport");
-		void GetNewStatusReport(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(GetUpdateMetadata, L"getUpdateMetadata");
-		winrt::fire_and_forget GetUpdateMetadata(CodePushUpdateState updateState, winrt::Microsoft::ReactNative::ReactPromise<winrt::Windows::Data::Json::IJsonValue> promise) noexcept;
-
-		REACT_METHOD(IsFailedUpdate, L"isFailedUpdate");
-		void IsFailedUpdate(std::wstring packageHash, winrt::Microsoft::ReactNative::ReactPromise<bool> promise) noexcept;
-
-		REACT_METHOD(RestartApp, L"restartApp");
-		void RestartApp(bool onlyIfUpdateIsPending, winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(IsFirstRun, L"isFirstRun");
-		winrt::fire_and_forget IsFirstRun(std::wstring packageHash, winrt::Microsoft::ReactNative::ReactPromise<bool> promise) noexcept;
-
-		REACT_METHOD(NotifyApplicationReady, L"notifyApplicationReady");
-		void NotifyApplicationReady(winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue>&& promise) noexcept;
-
-		REACT_METHOD(DownloadUpdate, L"downloadUpdate");
-		winrt::fire_and_forget DownloadUpdate(winrt::Microsoft::ReactNative::JSValueObject updatePackage, bool notifyProgress, winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue> promise) noexcept;
-
-		REACT_METHOD(InstallUpdate, L"installUpdate");
-		winrt::fire_and_forget InstallUpdate(winrt::Microsoft::ReactNative::JSValueObject updatePackage, int installMode, int minimumBackgroundDuration, winrt::Microsoft::ReactNative::ReactPromise<winrt::Microsoft::ReactNative::JSValue> promise) noexcept;
-
-		winrt::Windows::Foundation::IAsyncAction CodePush::CodePush::InstallPackage(winrt::Microsoft::ReactNative::JSValueObject updatePackage);
+        void Allow() noexcept;
 
 		REACT_METHOD(ClearPendingRestart, L"clearPendingRestart");
-		void ClearPendingRestart(winrt::Microsoft::ReactNative::ReactPromise<void> promise) noexcept;
-		*/
+        void ClearPendingRestart() noexcept;
+
+		REACT_METHOD(Disallow, L"disallow");
+        void Disallow() noexcept;
+
+        /*
+         * This method is the native side of the CodePush.restartApp() method.
+         */
+		REACT_METHOD(RestartApp, L"restartApp");
+        void RestartApp(bool onlyIfUpdateIsPending) noexcept;
+
+        /*
+         * This method clears CodePush's downloaded updates.
+         * It is needed to switch to a different deployment if the current deployment is more recent.
+         * Note: we don’t recommend to use this method in scenarios other than that (CodePush will call this method
+         * automatically when needed in other cases) as it could lead to unpredictable behavior.
+         */
+		REACT_METHOD(ClearUpdates, L"clearUpdates");
+        void ClearUpdates() noexcept;
+
+    // #pragma mark - JavaScript-exported module methods (Private)
+
+        /*
+         * This method is the native side of the CodePush.downloadAndReplaceCurrentBundle()
+         * method, which replaces the current bundle with the one downloaded from
+         * removeBundleUrl. It is only to be used during tests and no-ops if the test
+         * configuration flag is not set.
+         */
+		REACT_METHOD(DownloadAndReplaceCurrentBundle, L"downloadAndReplaceCurrentBundle");
+        void DownloadAndReplaceCurrentBundle(wstring remoteBundleUrl) noexcept;
+
+        /*
+         * This method is checks if a new status update exists (new version was installed,
+         * or an update failed) and return its details (version label, status).
+         */
+		REACT_METHOD(GetNewStatusReport, L"getNewStatusReport");
+        void GetNewStatusReport(ReactPromise<JsonObject> promise) noexcept;
+
+		REACT_METHOD(RecordStatusReported, L"recordStatusReported");
+        void RecordStatusReported(JsonObject statusReport) noexcept;
+
+		REACT_METHOD(SaveStatusReportForRetry, L"saveStatusReportForRetry");
+        void SaveStatusReportForRetry(JsonObject statusReport) noexcept;
+
 	};
 }
