509befe02a48bc710525f79dfb379e7ff540ba46
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-10-21 12:24:42 -0700

    Modify App.js

--- Examples/CodePushDemoAppCpp/App.js
+++ Examples/CodePushDemoAppCpp/App.js
@@ -108,7 +108,7 @@ class App extends Component<{}> {
 					  Welcome to CodePush!
 					</Text>
 					<Text style={styles.welcome}>
-						Release bundle 3:42 PM
+						Demo bundle
 					</Text>
 					<TouchableOpacity onPress={this.restartApp.bind(this)}>
 						<Text style={styles.syncButton}>Press to restart</Text>
