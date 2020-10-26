cda8f395669e43cdf3179548897210f7fccc54ff
alexmoroz15 <aleksey.morozov@hotmail.com>
2020-10-26 15:50:22 -0700

    Add CodePushDemoAppCpp2

---
+++ Examples/CodePushDemoAppCpp2/windows/CodePushDemoAppCpp2/MainPage.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+
+namespace winrt::CodePushDemoAppCpp2::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CodePushDemoAppCpp2::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
+
